98292dbe02b53fe9581739c74c4648960f00c45a
femellone <38559436+femellone@users.noreply.github.com>
2023-05-04 00:22:17 -0400

    add clamp to the modal subtitle

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -30,7 +30,7 @@
 		class="bg-white rounded-lg shadow-xl w-full max-w-2xl mx-auto p-4 md:p-8 text-left relative text-black"
 	>
 		<h1>{title}</h1>
-		<p>{subTitle}</p>
+		<p class="line-clamp-3">{subTitle}</p>
 		<button
 			type="button"
 			on:click={() => {
