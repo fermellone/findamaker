ed45fe2456a5854ddb806e17094a2f3af51c6e40
femellone <38559436+femellone@users.noreply.github.com>
2023-05-06 22:47:51 -0400

    testing

---
+++ src/lib/analytics-events.ts
@@ -0,0 +1,11 @@
+export const analyticsEvents = {
+	SIGNIN_SUCCESSFULLY: 'signin',
+	SIGNUP_SUCCESSFULLY: 'signup',
+	SIGNOUT_SUCCESSFULLY: 'signout',
+	SIGNIN_FAILED: 'signin_failed',
+	SIGNUP_FAILED: 'signup_failed',
+	SIGNOUT_FAILED: 'signout_failed',
+	UPVOTE_WITHOUT_SIGNIN: 'upvote_without_signin',
+	CREATE_PROBLEM_WITHOUT_SIGNIN: 'create_problem_without_signin',
+	CLICK_ON_PROBLEM_DETAILS: 'click_on_problem_details'
+};
