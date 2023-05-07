1aeacd6c3bc256003a8b67373988ef751fe043c4
femellone <38559436+femellone@users.noreply.github.com>
2023-05-07 04:53:58 -0400

    remove analytics completely

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,52 +1,6 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-
-	// const meassurementId = 'G-DJ2HTPTK1Z';
-
-	// $: {
-	// 	if (typeof gtag !== 'undefined') {
-	// 		gtag('config', meassurementId, {
-	// 			page_title: document.title,
-	// 			page_path: $page.url.pathname
-	// 		});
-	// 	}
-	// }
-
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
-
-<svelte:head>
-	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id={meassurementId}">
-	</script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-DJ2HTPTK1Z');
-	</script> -->
-
-	<!-- Google Tag Manager -->
-	<!-- Google Tag Manager -->
-	<script>
-		(function (w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-MC6KZNK');
-	</script>
-	<!-- End Google Tag Manager -->
-</svelte:head>
-
-
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import Analytics from '$lib/components/Analytics.svelte';
 	import '../app.postcss';
 </script>
