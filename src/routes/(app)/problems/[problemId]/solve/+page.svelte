b2f8b157bba5939188905bf06ed82e79076fa02e
femellone <38559436+femellone@users.noreply.github.com>
2023-05-08 01:20:25 -0400

    remove the link validation

--- src/routes/(app)/problems/[problemId]/solve/+page.svelte
+++ src/routes/(app)/problems/[problemId]/solve/+page.svelte
@@ -22,7 +22,7 @@
 		await fetch(`/api/problems/${problem!.id}/solutions`, {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ ...data, authorId: $userState!.id })
+			body: JSON.stringify({ ...data, authorId: $userState!.id, link: `https://${linkInput}` })
 		});
 		goto(`/`);
 	};
