89840699270bbeb8ac7f3e1e569bd488b51cc656
femellone <38559436+femellone@users.noreply.github.com>
2023-08-30 00:07:38 -0400

    remove the twitter option

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -88,13 +88,13 @@
 								: 'default'} desktop inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
 							aria-current="page">Solutions</a
 						>
-						<a
+						<!-- <a
 							href="/twitter-problems"
 							class="{$page.url.pathname === '/twitter-problems'
 								? 'current'
 								: 'default'} desktop inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
 							>Twitter problems</a
-						>
+						> -->
 						{#if $userState}
 							<a
 								href="/mine"
@@ -224,13 +224,13 @@
 							: 'default'} mobile block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
 						aria-current="page">Solutions</a
 					>
-					<a
+					<!-- <a
 						href="/twitter-problems"
 						class="{$page.url.pathname === '/twitter-problems'
 							? 'current'
 							: 'default'} mobile block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
 						>Twitter problems</a
-					>
+					> -->
 					{#if $userState}
 						<a
 							href="/mine"
